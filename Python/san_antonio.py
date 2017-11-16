d2ea2c1590ac62f9d1c60bbd7c8dba3fe28de3b6
LeothosThoren <soussou.med@live.fr>
2017-11-16 17:56:56 +0100

    san_antonio.py opérationnel mais pas optimisé

--- Python/san_antonio.py
+++ Python/san_antonio.py
@@ -1,34 +1,26 @@
 # -*- coding: utf8 -*-
-quotes = [
-    "Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !", 
+import random
+
+quotes = ["Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !", 
     "On doit pouvoir choisir entre s'écouter parler et se faire entendre."
 ]
 
-characters = [
-    "alvin et les Chipmunks", 
-    "Babar", 
-    "betty boop", 
-    "calimero",
-    "casper", 
-    "le chat potté", 
-    "Kirikou"
-]
+characters = ["alvin et les Chipmunks", "Babar", "betty boop", "calimero", "casper", "le chat potté", "Kirikou"]
 
-def get_random_quote(my_list):
-    #TODO
-    item = my_list[0]
-    return item
+def get_random_elements(my_list1, my_list2):
+
+    rand_charac = random.randint(0, len(my_list1) - 1)
+    rand_quote = random.randint(0, len(my_list2) - 1)
+    item1 = my_list1[rand_charac]
+    item2 = my_list2[rand_quote]
+    return ">>> {} a dit un jour : {}\n".format(item1.capitalize(), item2)
 
 user_answer = input("Tapez entrée pour découvrir une autre citation ou " +
-                    "taper 'B' pour quitter le programme :")
+                    "taper 'q' pour quitter le programme :")
 
-while user_answer != "B":
-    print(get_random_quote(quotes))
+while user_answer != "q":
+    print(get_random_elements(characters, quotes))
     user_answer = input("Tapez entrée pour découvrir une autre citation ou " +
-                    "taper 'B' pour quitter le programme :")
+                    "taper 'q' pour quitter le programme :\n")
 
-for character in characters:
-   var =  character.capitalize()
-   print(var)
 
-print(get_random_quote(quotes))
