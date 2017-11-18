ed53ddff2a04c3c6a505f4d6cdf6df5f9f1d1c70
Sofiane M <LeothosThoren@users.noreply.github.com>
2017-11-18 02:18:57 +0100

    No use

--- Python/characters_scrapy.py
+++
@@ -1,9 +0,0 @@
-import scrapy
-
-class BlogSpider(scrapy.Spider):
-    name = 'characterspider'
-    start_urls = ['http://www.topito.com/top-meilleurs-personnages-film']
-
-    def parse(self, response):
-        for characters in response.css('div.col-main li'):
-            yield {'character': characters.css('strong ::text').extract_first()}
